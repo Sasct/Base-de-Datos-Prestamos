a8058c4edab5595996d39253c96e8b208cd446b6
Sasct <freyderbejaranoduvan@gmail.com>
2026-08-24 19:49:29 -0500

    Add files via upload

--- Ejercicio_Prestamo.sql
+++ Ejercicio_Prestamo.sql
@@ -53,4 +53,108 @@ INSERT INTO Prestamo (Fecha_Devolucion, Fecha_Prestamo, ID_Equipo, ID_Estudiante
 ('2026-09-02', '2026-08-25 11:20:00', 5, 5),
 ('2026-09-05', '2026-08-26 13:10:00', 6, 6);
 
+#Codigo para buscar en la base de datos
+#Mostar Nombre y Apellidos de estudiante
+SELECT Nombre, Apellido
+FROM estudiante;
 
+#Mostrar una caracteristica en especial
+SELECT Marca, Modelo, Estado
+FROM equipo
+WHERE Estado = "Disponible";
+
+#Mostrar el codigo inventario, marca y estado de los equipos en mantenimiento
+SELECT Codigo_Inventario, Marca, Estado
+FROM equipo
+WHERE Estado = "Mantenimiento"
+
+#Mostar el Apellido, Nombre y correo de los estuidantes de Ingenieria de Sistemas
+SELECT Apellido, Nombre, Correo
+FROM estudiante
+WHERE Programa = "Ingenieria de Sistemas"
+
+#Consultar la informacion correspondiente al estudiante con identificacion 103
+SELECT *
+FROM estudiante
+WHERE ID_Estudiante = "103"
+
+#Busque el equipo por codigo de inventario PC-003 Y muestre identificador, marca y estado
+SELECT ID_Equipo, Marca, Estado
+FROM equipo
+WHERE Codigo_Inventario = "PC-003"
+
+#Codigo para buscar cuando se cumplen dos condiciones (and)
+SELECT *
+FROM equipo
+WHERE marca = "LENOVO" AND estado = "Disponible";
+
+#Codigo para buscar cuando una de las dos condiciones se cumple
+(OR)
+SELECT *
+FROM equipos
+WHERE marca = "LENOVO" OR marca = "HP";
+
+#Codigo para buscar entre dos datos
+SELECT *
+FROM equipo
+WHERE Marca IN("LENOVO", "HP"); 
+
+#Codigo para organizar de forma ascendente o descendente
+SELECT Apellido, Nombre
+FROM estudiante
+ORDER BY Apellidos DESC;
+
+#Mostrar Nombre, Apellido, programa de los estudiantes y ordemar de forma ascendente el nombre
+SELECT Apellido, Nombre, Programa
+FROM estudiante
+ORDER BY Nombre ASC;
+
+#Ordene de forma descendente por Identificador los estudiantes y muestre todos sus datos
+SELECT *
+FROM Estudiante
+ORDER BY ID_Estudiante DESC;
+
+#Codigo para buscar un dato si no se sabe como esta escrito o que tengan alguna cosa que este escrita en el parentesis
+
+#Que tenga "LENOVO" ya sea en Mayusculas o Minusculas
+SELECT *
+FROM equipo
+WHERE Marca LIKE "LENOVO"
+
+#Que tenga L en algun lado
+SELECT *
+FROM equipo
+WHERE Marca LIKE "%L%"
+
+#Que tenga L al final
+SELECT *
+FROM equipo
+WHERE Marca LIKE "%L"
+
+#Que tenga L al inicio
+SELECT *
+FROM equipo
+WHERE Marca LIKE "L%"
+
+#Que L este en la 4 posicion
+SELECT *
+FROM equipo
+WHERE Marca LIKE "___L"
+
+#Consulte los nombres de estudiantes que inicien por la A
+SELECT Nombre
+FROM estudiante
+WHERE Nombre LIKE "A%"
+
+#Funciones de agregado
+#COUNT() contar
+#Determine cuantos computadores se encuentran disponibles
+SELECT COUNT(*)
+FROM equipo
+WHERE estado = "Disponible";
+#MAX, MIN, AVG
+
+#Cuente los estudiantes del programa ingenieria de software
+SELECT COUNT(*)
+FROM estudiante
+WHERE Programa = "Ingenieria de software"
